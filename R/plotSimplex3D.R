a2a07e02ecdbef53c3e6f1e3a13bb93cfa67c065
Crparedes <craparedesca@unal.edu.co>
2020-05-23 01:41:39 -0500

    weird lines fixed for the 3D representations

--- R/plotSimplex3D.R
+++ R/plotSimplex3D.R
@@ -1,62 +1,61 @@
-#' Makes a three dimentional plot of a simplex object.
+#' Draws a three dimentional plot of the vertexes in a simplex
 #'
-#' The function generates a 3D plot for a \code{smplx} object having
-#' dimensionality of at least 3. When dimensionality is higher than 3, the
-#' function plots a 3D proyection of selected dimensions.
+#' The function generates a 3D plot of the vertexes in a simplex optimization
+#' when simplex dimensionality is at least 3. When dimensionality is higher
+#' than 3, the plot produced is a proyection of the selected variables.
 #'
-#' @param  sel.dim numeric or char vector for variables to be considered when
-#'                 simplex dimensionality is higher than 3. If \code{numeric}
-#'                 form it must contain dimensions ordinal number. If
-#'                 \code{char}, it must contain dimensions names.
+#' @param  sel.dim numeric or char vector for the variables to be considered
+#'                 when simplex dimensionality is higher than 3. If it is in
+#'                 \code{numeric} format, must contain the ordinal number of
+#'                 the variables. If the format is in \code{char} vector,
+#'                 must contain variable names.
 #' @param  main    title for the plot.
 #' @param  angle   angle for perspective between x and y axis.
-#' @param  ...     other arguments passed to \code{scatterplot3d::scatterplot3d}
+#' @param  ...     other arguments passed to
+#'                 \code{\link[scatterplot3d]{scatterplot3d}}
 #' @inheritParams generateVertex
 #' @inheritParams plot.smplx
 #' @return 3D proyection of the simplex coordinates.
 #' @examples
-#'   plotSimplex3D(simplex = labsimplex(N = 3))
+#'   plotSimplex3D(simplex = labsimplex(N = 3, centroid = c(350, 11, 0.7),
+#'                                      stepsize = c(10, 0.5, 0.1),
+#'                                      var.name = c('temperature', 'pH',
+#'                                                   'concentration')))
 #'
+#'   ## Several options are posible when visualizing higher order simplexes
 #'   plotSimplex3D(simplex = labsimplex(N = 8))
 #'   plotSimplex3D(simplex = labsimplex(N = 8), sel.dim = c(4, 6, 8))
 #'
-#'   ## Simulation of the real proccess where a simplex is made and measured,
-#'   ## plotted nad the new vertex is measuresd after it is generated.
-#'   set.seed(12)
-#'   simplex3D <- labsimplex(N = 3)
-#'   plotSimplex3D(simplex = simplex3D)
-#'   generateVertex(simplex = simplex3D, qflv = rnorm(4), overwrite = TRUE)
-#'   plotSimplex3D(simplex = simplex3D)
-#'   generateVertex(simplex = simplex3D, qflv = rnorm(1), overwrite = TRUE)
-#'   plotSimplex3D(simplex = simplex3D)
-#'   generateVertex(simplex = simplex3D, qflv = rnorm(1), overwrite = TRUE)
-#'   plotSimplex3D(simplex = simplex3D)
+#'   ## Simplex movements can be visualized after some experiments has been
+#'   ## performed
+#'   simplex <- exampleOptimization(surface = exampleSurfaceR3,
+#'                                  centroid = c(350, 11, 0.7),
+#'                                  stepsize = c(10, 0.5, 0.1),
+#'                                  experiments = 15)
+#'   plotSimplex3D(simplex = simplex)
+#' @seealso \code{\list{plot.smplx}}
 #' @importFrom graphics lines par plot segments title
 #' @import scatterplot3d
-#'
 #' @author Cristhian Paredes, \email{craparedesca@@unal.edu.co}
 #' @author Jesús Ágreda, \email{jagreda@@unal.edu.co}
 #' @export
 
 plotSimplex3D <- function(simplex, sel.dim = NULL, all.ver = TRUE,
-                          all.lin = TRUE, main = NULL, angle = 30, ...){
+                          all.lin = TRUE, main = NULL, angle = 30, ...) {
 
   # Error handling
   checkMain(simplex = simplex)
-  if (simplex$dim < 3){
-    stop("Simplex dimension must be at least 3")
-  }
+  if (simplex$dim < 3) stop("Simplex dimension must be at least 3")
 
   var.plt <- 1:3
   if (!missing(sel.dim)) {
     if (length(sel.dim) != 3) {
-      stop("Only 3 coordinates can be plotted. Length of sdim vector differs
-           from 3: ", length(sel.dim))
+      stop("Only 3 coordinates can be plotted. Length of sdim vector differs",
+           "from 3: ", length(sel.dim))
       }
   } else {
     if (simplex$dim > 3) {
-      message("No selected dimensions for ploting. Default is the first three
-              ones.")
+      message("No selected dimensions for ploting. Default is the first three ones.")
     }
   }
 
@@ -75,35 +74,52 @@ plotSimplex3D <- function(simplex, sel.dim = NULL, all.ver = TRUE,
     }
   }
 
-
   if (all.ver) {
-    s <- scatterplot3d::scatterplot3d(x = simplex$coord[, var.plt], type = 'p',
-                                      pch = 16, grid = FALSE, angle = angle,
-                                      main = main, lty.hide = 3, ...)
+    s <- scatterplot3d(x = simplex$coord[, var.plt], type = 'p', pch = 16,
+                       grid = FALSE, angle = angle, main = main,
+                       lty.hide = 3, ...)
   } else {
-    s <- scatterplot3d::scatterplot3d(x = simplex$coord[(nrow(simplex$coords) -
-             simplex$dim):nrow(simplex$coords), var.plt], type = 'p', pch = 16,
-             grid = FALSE, angle = angle, main = main, lty.hide = 3, ...)
+    s <- scatterplot3d(x = simplex$coord[(nrow(simplex$coords) -
+                       simplex$dim):nrow(simplex$coords), var.plt],
+                       type = 'p', pch = 16, grid = FALSE, angle = angle,
+                       main = main, lty.hide = 3, ...)
   }
   vertex.xy <- cbind(s$xyz.convert(simplex$coord[, var.plt[1]],
-                       simplex$coord[,var.plt[2]],
-                       simplex$coord[, var.plt[3]])$x,
+                     simplex$coord[, var.plt[2]],
+                     simplex$coord[, var.plt[3]])$x,
                      s$xyz.convert(simplex$coord[, var.plt[1]],
-                       simplex$coord[, var.plt[2]],
-                       simplex$coord[, var.plt[3]])$y)
+                     simplex$coord[, var.plt[2]],
+                     simplex$coord[, var.plt[3]])$y)
+  #if (all.lin) {
+  #  for (ii in 1:(nrow(simplex$coords) - simplex$dim)) {
+  #    segments(x0 = vertex.xy[ii, 1], x1 = vertex.xy[(ii + 1):(ii + 3), 1],
+  #             y0 = vertex.xy[ii, 2], y1 = vertex.xy[(ii + 1):(ii + 3), 2],
+  #             col = "grey", lwd = 0.7)
+  #  }
+  #}
   if (all.lin) {
-    for (ii in 1:(nrow(simplex$coords) - simplex$dim)) {
-      segments(x0 = vertex.xy[ii, 1], x1 = vertex.xy[(ii + 1):(ii + 3), 1],
-               y0 = vertex.xy[ii, 2], y1 = vertex.xy[(ii + 1):(ii + 3), 2],
-               col = "grey", lwd = 0.7)
+    V.pos <- as.numeric(gsub("Vertex.", "", row.names(simplex$coords)))
+    #if (!is.null(nrow(x$families))) {
+    for (ii in 1:length(simplex$families)) {
+      for (jj in 1:(length(simplex$families[[ii]]) - 1)) {
+        for (kk in (jj + 1):length(simplex$families[[ii]])) {
+          jj. <- which(simplex$families[[ii]][jj] == V.pos)
+          kk. <- which(simplex$families[[ii]][kk] == V.pos)
+          lines(vertex.xy[c(jj., kk.),  var.plt[1]],
+                vertex.xy[c(jj., kk.),  var.plt[2]], col = "grey")
+          #segments(x0 = vertex.xy[ii, 1], x1 = vertex.xy[(ii + 1):(ii + 3), 1],
+          #         y0 = vertex.xy[ii, 2], y1 = vertex.xy[(ii + 1):(ii + 3), 2],
+          #         col = "grey", lwd = 0.7)
+        }
+      }
     }
+    #}
   }
 
   for (ii in (nrow(simplex$coords) - simplex$dim):(nrow(simplex$coords) - 1)) {
-    segments(x0 = vertex.xy[ii, 1], x1 = vertex.xy[(ii + 1):
-                                                     nrow(simplex$coords), 1],
-             y0 = vertex.xy[ii, 2], y1 = vertex.xy[(ii + 1):
-                                                     nrow(simplex$coords), 2],
-             col = "blue")
+    segments(x0 = vertex.xy[ii, 1],
+             x1 = vertex.xy[(ii + 1):nrow(simplex$coords), 1],
+             y0 = vertex.xy[ii, 2],
+             y1 = vertex.xy[(ii + 1):nrow(simplex$coords), 2], col = "blue")
   }
 }
